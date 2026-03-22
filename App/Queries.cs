8fbc36db73f7b00a6493305621bfbcd8adaf605a
SebasT807 <sebastian.dev0708@gmail.com>
2026-03-22 14:33:15 -0500

    feat: Add BetterConsole dependency

--- App/Queries.cs
+++ App/Queries.cs
@@ -1,5 +1,7 @@
 namespace App;
 
+using BetterConsoles.Tables;
+
 public class Queries(List<Task> _tasks)
 {
     private readonly List<Task> Tasks = _tasks;
@@ -7,15 +9,8 @@ public class Queries(List<Task> _tasks)
     // métodos para gestionar las tareas 
     public void ListTask()
     {
-        ForegroundColor = ConsoleColor.DarkBlue;
-        WriteLine("-----Lista de tareas-----");
-        WriteLine("\n{0,-8} {1,-35} {2,-15}", "Id", "Descripcion", "Completado");
-        foreach (Task task in Tasks)
-        {
-            WriteLine(new string('-', 58));
-            WriteLine("\n{0,-8} {1,-35} {2,-15}", task.Id, task.Description, task.Completed ? "Si" : "No");
-
-        }
+        Table table = new Table("Id", "Descripcion", "Completado");
+        
     }
 
 }
